e5c40acf6d1c1730e4da972a3c9d735936478205
Shaun Khundker <shaun.khundker@gmail.com>
2024-10-15 18:33:03 -0400

    autofmt solidity

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+--
+local forge_fmt_group = vim.api.nvim_create_augroup("autocmds for forge", { clear = true })
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  pattern = { "*.sol" },
+  desc = "Auto-format solidity files after saving",
+  callback = function()
+    vim.cmd("silent! !forge fmt")
+  end,
+  group = forge_fmt_group,
+})
