3f3b673f1d718c2ec495a92b35fabcfe7c39f82e
shaunkh <13260261+shaunkh@users.noreply.github.com>
2024-11-11 22:49:31 -0500

    x

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,7 @@
+return {
+  { "mason-lspconfig.nvim", enabled = false },
+  { "mason.nvim", enabled = false },
+  -- { "nvim-treesitter", enabled = false },
+  { "nvim-ts-autotag", enabled = false },
+  { "nvim-treesitter-context", enabled = false },
+}
